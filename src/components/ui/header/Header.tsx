c4c173cbcd2f8120cf8f1f3bc800c5376223730b
NikitaBigHead <ggfgygi1@gmail.com>
2022-10-11 11:50:33 +0300

    get config

--- src/components/ui/header/Header.tsx
+++ src/components/ui/header/Header.tsx
@@ -1,26 +1,21 @@
+import "./styles.css";
 import React from "react";
-import {Layout,Menu} from"antd";
-import "antd/dist/antd.css";
-import "./header.css";
-import Item from "./menu.item/Item";
+import { NavLink } from "react-router-dom";
 
-const {Header} = Layout;
 
-const _Header=()=>{
+
+const Header=()=>{
     return(
-        <Layout className="layout">
-            <Header className="Header">
-                <div className="logo" onClick={()=>document.location  = "/"}>TestYou</div>
-                <Menu className="Menu"
-                    theme="light"
-                    mode="horizontal"
-                    defaultSelectedKeys={['']}>
-                    <Item key = "1" url = "/create" text = "Создать"/>
-                    <Item key = "2" url = "/tests" text = "Создать"/>
-                    <Item key = "3" url = "/login" text = "Создать"/>
-                </Menu>
-            </Header>
-        </Layout>
+        <div className="ho">
+            <header className="header">
+                <a className="logo" href = "#">TestYou</a>
+                <nav className="headerNav">
+                    <NavLink style={{    padding:"1rem 5rem", textDecoration:"none",color:"black"}} to ="/tests">Тесты</NavLink>
+                    <NavLink className={"item"} to ="/create">Создать</NavLink>
+                    <NavLink className={"item"} to ="/login">Войти</NavLink>
+                </nav>
+            </header>
+        </div>
     )
 }
-export default _Header;
+export default Header;
